1f3306c91662083d73f877066298d9b49165d73b
nkatha <125828300+nkatha1@users.noreply.github.com>
2024-06-13 13:05:09 +0300

    Add files via upload

--- SQL PROJECT1.sql
+++ SQL PROJECT1.sql
@@ -62,5 +62,68 @@ values(6,"js","ax","2024-05-12",5.00); -- under hourly pay of 10.00
 /* to delete a check*/
 ALTER TABLE employee
 DROP CHECK chk_hourly_pay;
-SELECT * FROM employee; 
+SELECT * FROM employee;
+/* DEFAULT CONSTRAINT */
+/*In this one we don't exclusively set something*/
+SELECT * FROM employee;
+INSERT INTO employee
+values (7, "peshy","vic",2024-08-09,21.00),
+		(8,"wambo","ashley", 2024-09-08, 22.00);
+SELECT * FROM employee;
+CREATE TABLE products(
+			stationary_id INT,
+            stationary_name VARCHAR (20),
+           stationary_price DECIMAL (4,2) NOT NULL);
+SELECT * FROM products;
+INSERT INTO products
+VALUES (2, "pens", 2.04),
+		(3, "pencils", 3.04),
+        (4, "rubbers", 4.04);
+SELECT * FROM products;
+/* HOW TO DELETE FROM A TABLE */
+DELETE FROM products
+WHERE stationary_price >= 3.04;
+SELECT * FROM products;
+
+/* TO INCLUDE A CONSTRAINT TO AN ALREADY EXISTING TABLE*/
+ALTER TABLE products
+ALTER stationary_name set default 0;
+SELECT * FROM products;
+
+/* PRIMARY KEY CONSTRAINT (UNIQUE IDENTIFIER) /*
+/* TO ADD A PRIMARY KEY TO A TABLE THAT ALREADY EXISTS 
+you cannot have two rows with the same values*/
+ALTER TABLE products
+ADD CONSTRAINT
+PRIMARY KEY (stationary_id);
+SELECT * FROM products;
+/* inserting new values in the existing table */
+INSERT INTO products
+values( 3, "rubbers", 3.04),
+		( 4, "sharpener", 2.01);
+SELECT * FROM products;
+/* shows the column stationary price with the specified price */
+SELECT stationary_price 
+FROM products
+WHERE stationary_price = 3.04;
+
+/* AUTO INCREMENT */
+/* WHEREVER WE INPUT A NEW KEY IN THIS IT INCREASES BY ONE */
+CREATE TABLE transcations (
+			transcations_id INT PRIMARY KEY auto_increment,
+            amount decimal (6,3)
+            );
+SELECT * FROM transcations;
+INSERT INTO transcations
+values( 1,5.99);
+select * from transcations;
+INSERT INTO transcations (amount)
+values ( 6.99);
+select * from transcations;
+INSERT INTO transcations (amount)
+value (7.99);
+select * from transcations;
+
+            
+
 
